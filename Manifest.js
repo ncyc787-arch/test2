14a741b4cf4008a6b0dc8e78907da0d825f9d28f
ncyc787-arch <ncyc787@gmail.com>
2026-04-20 22:12:53 +0500

    Create Manifest.js

---
+++ Manifest.js
@@ -0,0 +1,12 @@
+{
+    "display_name": "PhoneMSG",
+    "loading_order": 15,
+    "requires": [],
+    "optional": [],
+    "js": "index.js",
+    "css": "style.css",
+    "author": "ncyc787-arch",
+    "version": "0.5.0",
+    "homePage": "https://github.com/ncyc787-arch/test",
+    "auto_update": true
+}
